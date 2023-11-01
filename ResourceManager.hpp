9552a2ad6b8a66b9647717147c8c02acab319ef7
bujniasz <149008651+bujniasz@users.noreply.github.com>
2023-11-01 20:16:22 +0100

    Update ResourceManager.hpp
    v1 - 01.11.23

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,39 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+        public:
+        //kontruktor domyslny, czerpiacy z Resource
+        ResourceManager() : res() {}
+
+        //funkcja zwracajaca wartosc metody get dla obiektu klasy Resource
+        double get(Resource obiekt)
+        {
+            return obiekt.get();
+        }
+
+        //METODY SPECJALNE:
+
+        //1.Destruktor
+        ~ResourceManager()
+        {
+            //std::cout << "destruktor nisczy obiekt" << "\n";
+        }
+
+        //2.Konstruktor kopiujacy
+        ResourceManager(const ResourceManager& rm)
+        {
+
+        }
+
+        //3.Kopiujacy operator przypisania
+        ResourceManager& operator=(const ResourceManager& rm)
+        {
+            
+        }
+
+
+
+
+    private:
+        Resource res; //zarządzany obiekt klasy resource
 };
