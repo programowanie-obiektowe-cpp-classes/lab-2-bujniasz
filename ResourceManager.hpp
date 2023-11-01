66d086852fae4c3d0176833540ddca83f0d1036f
bujniasz <149008651+bujniasz@users.noreply.github.com>
2023-11-01 22:34:00 +0100

    Update ResourceManager.hpp
    v7 - zad 1 ze wsk inteligentnymi - 01.11.23

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,23 +1,20 @@
 #pragma once
 
 #include "Resource.hpp"
+#include <memory> //biblioteka do wskaznikow inteligentnych
 
 class ResourceManager
 {
     public:
-        //kontruktor domyslny, czerpiacy z Resource
-        ResourceManager() : res() {}
+        //kontruktor domyslny, czerpiacy z Resource, z ucyiem wsk intelegentnego
+        ResourceManager() : res(std::make_unique<Resource>()) {}
 
         //funkcja zwracajaca wartosc metody get dla obiektu klasy Resource
         double get()
         {
-            return res.get();
-        }
-
-        ~ResourceManager()
-        {
+            return res->get();
         }
 
     private:
-        Resource res; //zarządzany obiekt klasy resource
+        std::unique_ptr<Resource> res; //zarządzany obiekt klasy resource - wskaznik inteligentny
 };
